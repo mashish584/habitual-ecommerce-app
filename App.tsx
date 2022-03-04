8d686cb888bb5199a82a9935f8a7bb69da679b79
Ashish Mehra <sdashish.am@gmail.com>
2022-03-04 23:11:24 +0530

    Crashlytics log ~ removed

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 import "react-native-gesture-handler";
-import crashlytics from "@react-native-firebase/crashlytics";
 
 import Navigation from "./src/navigation";
 
 const App = () => {
-	useEffect(() => {
-		crashlytics().log("App Mounted");
-	}, []);
-
 	return (
 		<SafeAreaProvider>
 			<Navigation />
